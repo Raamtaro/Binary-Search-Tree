1d2ad79fae270e1b0e013f670330159fc6758aa1
Raam <raam.sanghani@gmail.com>
2024-04-11 14:00:41 -0500

    setting up the skeleton

---
+++ bstClass.js
@@ -0,0 +1,16 @@
+import Node from "./nodeClass.js"
+
+class Tree {
+
+    constructor(startingArray = []){
+        this.startingArray = startingArray
+        this.root = null
+
+    }
+
+    buildTree(array) {
+
+    }
+}
+
+export default Tree
---
+++ nodeClass.js
@@ -0,0 +1,9 @@
+class Node {
+    constructor(data = null, right = null, left = null) {
+        this.data = data
+        this.right = right
+        this.left = left
+    }
+}
+
+export default Node
