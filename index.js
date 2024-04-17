9cf325b76750e21e99413022430c2f19d3bd90a1
Raam <raam.sanghani@gmail.com>
2024-04-17 13:27:14 -0500

    Finished driver script.

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 import Tree from "./classes/bstClass.js";
 import prettyPrint from "./misc/prettyPrint.js";
+import { randomArray, cryptoRandomArray, cryptoRandomAdditions } from "./misc/randomArray.js"; //Two different methods of generating random numbers
+
+const myTree = new Tree(cryptoRandomArray()) //Creating Tree out of 25 random numbers that are less than 100
+const checkBalance = bsTree => {return bsTree.isBalanced() ? "Balanced" : "Unbalanced" } //A little redundant, but just a helper
+const treeAdditions = cryptoRandomAdditions(7) //Preparing an array of 7 random numbers greater than 100 to later unbalance the tree
 
 
+console.log("*******************STARTING TREE***************************")
+
+prettyPrint(myTree.root)
+console.log(checkBalance(myTree))
+
+console.log("*******************BFS and DFS Analysis***************************")
+
+console.log("Breadth First Search")
+console.log(myTree.levelOrder())
+
+console.log("Depth First Search")
+console.log("PreOrder:", myTree.preOrder())
+console.log("InOrder:", myTree.inOrder())
+console.log("PostOrder:", myTree.postOrder())
+
+console.log("*******************UNBALANCING TREES***************************")
+
+treeAdditions.forEach(num => {
+
+    console.log(`inserting: ${num}`)
+    myTree.insert(num)
+})
+
+prettyPrint(myTree.root)
+
+console.log(`Following these insertions, this BST is currently: ${checkBalance(myTree)}`)
+
+
+console.log("*******************REBALANCED TREE***************************")
+prettyPrint(myTree.reBalance())
+
+console.log(`Tree is now ${checkBalance(myTree)}`)
+
+console.log("*******************FINAL BFS & DFS RESULTS***************************")
+
+console.log("Breadth First Search")
+console.log(myTree.levelOrder())
+
+console.log("Depth First Search")
+console.log("PreOrder:", myTree.preOrder())
+console.log("InOrder:", myTree.inOrder())
+console.log("PostOrder:", myTree.postOrder())
--- misc/randomArray.js
+++ misc/randomArray.js
@@ -16,4 +16,11 @@ const cryptoRandomArray = () => { //I went down a rabit-hole method
     return Array.from(array).map(int => int % 100)
 }
 
-export {randomArray, cryptoRandomArray}
+const cryptoRandomAdditions = (count) => { //Count is the number of additions that we'll be making to our tree
+
+    let array = new Uint8Array(count);  // create a typed array to hold 'count' 8-bit unsigned integers
+    crypto.getRandomValues(array); // fill the array with cryptographically strong random values
+    return Array.from(array).map(num => num % (256 - 101) + 101) // 256 because we are using 8-bit numbers
+}
+
+export {randomArray, cryptoRandomArray, cryptoRandomAdditions}
